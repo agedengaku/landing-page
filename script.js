08901031a54b8ecee16bb2a7953a6cd0010bcbb8
JM Taylor <jmotaylor2@gmail.com>
2018-12-09 13:18:51 +0900

    add carousel functionality and minor edits to form elements

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ var poochVids = document.getElementsByClassName("pooch-vids");
   });  
 });
 
+$('.single-item').slick({
+	dots: true,
+	infinite: true,
+	speed: 500,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 5000
+});
